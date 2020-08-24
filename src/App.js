9826061e6e2fc18dd2c440fc4b2b04b01ae25c88
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-08-24 17:55:42 +0200

    implement react-google-login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from "react";
 import "./App.css";
-
+import { GoogleLogin, GoogleLogout } from "react-google-login";
 function App() {
+  const responseGoogle = (response) => {
+    console.log(response);
+  };
+  const logout = () => {
+    console.log("logout");
+  };
+
   return (
     <div className="App">
       <h2>Hello react!</h2>
+      <GoogleLogin
+        clientId={process.env.REACT_APP_CLIENT_ID}
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={"single_host_origin"}
+        isSignedIn={true}
+      />
+      <GoogleLogout
+        clientId={process.env.REACT_APP_CLIENT_ID}
+        buttonText="Logout"
+        onLogoutSuccess={logout}
+      ></GoogleLogout>
     </div>
   );
 }
