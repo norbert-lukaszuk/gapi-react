6eace9683e5454791a4bf4bd47702733cee3f1b8
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-08-25 10:22:23 +0200

    get 10  calendar events

--- src/App.js
+++ src/App.js
@@ -2,7 +2,52 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const gapi = window.gapi;
+  const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const DISCOVERY_DOCS = [
+    "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+  ];
+  const SCOPES = "https://www.googleapis.com/auth/calendar.events";
+  const onClickHanler = () => {
+    gapi.load("client:auth2", () => {
+      console.log("loaded client");
+
+      gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES,
+      });
+
+      gapi.client.load("calendar", "v3", () => console.log("bam!"));
+      gapi.auth2
+        .getAuthInstance()
+        .signIn()
+        .then(() =>
+          gapi.client.calendar.events
+            .list({
+              calendarId: "primary",
+              timeMin: new Date().toISOString(),
+              showDeleted: false,
+              singleEvents: true,
+              maxResults: 10,
+              orderBy: "startTime",
+            })
+            .then((response) => {
+              const events = response.result.items;
+              console.log("EVENTS: ", events);
+            })
+        );
+    });
+  };
+  const onClickLogoutHandler = () => {};
+  return (
+    <div className="App">
+      <button onClick={onClickHanler}>Get calendar</button>
+      <button onClick={onClickLogoutHandler}>Logout</button>
+    </div>
+  );
 }
 
 export default App;
